6f3f796d9fbc2f1ecfdbc74d3e3aaf4f23eb59c4
Kelly Cheng <iluvpippi@gmail.com>
2016-04-25 23:00:09 -0400

    Local alarm initial commit, not tested yet

--- android project/ServletTest/app/src/main/java/com/wakiedokie/waikiedokie/ui/AlarmEditTimeActivity.java
+++ android project/ServletTest/app/src/main/java/com/wakiedokie/waikiedokie/ui/AlarmEditTimeActivity.java
@@ -1,13 +1,16 @@
 package com.wakiedokie.waikiedokie.ui;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import com.wakiedokie.waikiedokie.R;
+
+import java.util.Calendar;
 
 /**
  * Created by chaovictorshin-deh on 4/14/16.
@@ -16,9 +19,19 @@ public class AlarmEditTimeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_alarm);
+        setContentView(R.layout.activity_edit_alarm_time);
 
+        // Create an offset from the current time in which the alarm will go off
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.SECOND, 5);
 
+        final Intent intent = new Intent(AlarmEditTimeActivity.this, AlarmStatusActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+        AlarmManager am =
+                (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                pendingIntent);
 
         Button btn_select_buddy = (Button) findViewById(R.id.btn_to_select_buddy);
         btn_select_buddy.setOnClickListener(new View.OnClickListener() {
@@ -33,7 +46,6 @@ public class AlarmEditTimeActivity extends Activity {
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(AlarmEditTimeActivity.this, AlarmConfirmActivity.class);
                 startActivity(intent);
             }
         });
--- android project/ServletTest/app/src/main/java/com/wakiedokie/waikiedokie/ui/AlarmStatusActivity.java
+++ android project/ServletTest/app/src/main/java/com/wakiedokie/waikiedokie/ui/AlarmStatusActivity.java
@@ -1,10 +1,75 @@
 package com.wakiedokie.waikiedokie.ui;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.wakiedokie.waikiedokie.R;
+
+import java.io.IOException;
 
 /**
  * Created by chaovictorshin-deh on 4/14/16.
  */
 public class AlarmStatusActivity extends Activity {
+    private MediaPlayer mMediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+
+        Button btn_turn_off = (Button) findViewById(R.id.btn_turn_off);
+        btn_turn_off.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mMediaPlayer.stop();
+                Intent intent = new Intent(AlarmStatusActivity.this, AlarmMainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        playSound(this, getAlarmUri());
+    }
+
+    private void playSound(Context context, Uri alert) {
+        mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(context, alert);
+            final AudioManager audioManager = (AudioManager) context
+                    .getSystemService(Context.AUDIO_SERVICE);
+            if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+                mMediaPlayer.prepare();
+                mMediaPlayer.start();
+            }
+        } catch (IOException e) {
+            System.out.println("OOPS");
+        }
+    }
+
+    //Get an alarm sound. Try for an alarm. If none set, try notification,
+    //Otherwise, ringtone.
+    private Uri getAlarmUri() {
+        Uri alert = RingtoneManager
+                .getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alert == null) {
+            alert = RingtoneManager
+                    .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            if (alert == null) {
+                alert = RingtoneManager
+                        .getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+        return alert;
+    }
 
 }
