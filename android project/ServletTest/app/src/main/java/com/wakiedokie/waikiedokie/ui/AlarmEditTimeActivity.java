47117fdec851f4bb5ca123a0a4a132a41539d8b6
Kelly Cheng <iluvpippi@gmail.com>
2016-04-26 22:14:58 -0400

    Set alarm according to timepicker instead of 5 seconds

--- android project/ServletTest/app/src/main/java/com/wakiedokie/waikiedokie/ui/AlarmEditTimeActivity.java
+++ android project/ServletTest/app/src/main/java/com/wakiedokie/waikiedokie/ui/AlarmEditTimeActivity.java
@@ -4,9 +4,12 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TimePicker;
 
 import com.wakiedokie.waikiedokie.R;
 
@@ -16,22 +19,22 @@ import java.util.Calendar;
  * Created by chaovictorshin-deh on 4/14/16.
  */
 public class AlarmEditTimeActivity extends Activity {
+    private static final String TAG = "AlarmEditActivity";
+    private TimePicker alarmTimePicker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_alarm_time);
 
-        // Create an offset from the current time in which the alarm will go off
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.SECOND, 5);
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
 
-        final Intent intent = new Intent(AlarmEditTimeActivity.this, AlarmStatusActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,
-                12345, intent, PendingIntent.FLAG_CANCEL_CURRENT);
-        AlarmManager am =
-                (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
-        am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
-                pendingIntent);
+        // Create an offset from the current time in which the alarm will go off
+
+
+//        cal.add(Calendar.SECOND, 5);
+
+
 
         Button btn_select_buddy = (Button) findViewById(R.id.btn_to_select_buddy);
         btn_select_buddy.setOnClickListener(new View.OnClickListener() {
@@ -46,7 +49,28 @@ public class AlarmEditTimeActivity extends Activity {
         btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(intent);
+
+                Calendar cal = Calendar.getInstance();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    cal.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getHour());
+                    cal.set(Calendar.MINUTE, alarmTimePicker.getMinute());
+                }
+                else {
+                    cal.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+                    cal.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+                }
+
+                Intent mainIntent = new Intent(AlarmEditTimeActivity.this, AlarmMainActivity.class);
+                Intent alarmRingIntent = new Intent(AlarmEditTimeActivity.this, AlarmStatusActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(AlarmEditTimeActivity.this,
+                        12345, alarmRingIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+                AlarmManager am =
+                        (AlarmManager)getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+                Log.d(TAG, "Alarm is set");
+
+                startActivity(mainIntent);
             }
         });
     }
